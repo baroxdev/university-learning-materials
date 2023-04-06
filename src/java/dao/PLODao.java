7abec9562cd5fceb55b176e7dcaf01755c10d146
shinta79 <giahung112358@gmail.com>
2023-04-06 15:45:39 +0700

    change all try-catch to throws

--- src/java/dao/PLODao.java
+++ src/java/dao/PLODao.java
@@ -17,50 +17,48 @@ import utils.DBUtils;
  * @author giahu
  */
 public class PLODao {
+
     //lấy plo theo id (ko chắc có dùng ko :v)
     public static PLO getPLOById(String id) throws Exception {
         String query = "select * from Program_Learning_Objective where id = '?'";
         PLO plo = null;
         Connection con = DBUtils.makeConnection();
-        try {
-            PreparedStatement pre = con.prepareStatement(query);
-            pre.setString(1, id);
-            ResultSet rs = pre.executeQuery();
-            while (rs.next()) {
-                plo = new PLO();
-                plo.setId(rs.getInt("id"));
-                plo.setName(rs.getString("name"));
-                plo.setDescription(rs.getString("description"));
-                plo.setCreatedAt(rs.getDate("createdAt").toString());
-                plo.setUpdatedAt(rs.getDate("updatedAt").toString());
-            }
-        } catch (Exception e) {
-            System.out.println("Cant get plo");
+
+        PreparedStatement pre = con.prepareStatement(query);
+        pre.setString(1, id);
+        ResultSet rs = pre.executeQuery();
+        while (rs.next()) {
+            plo = new PLO();
+            plo.setId(rs.getInt("id"));
+            plo.setName(rs.getString("name"));
+            plo.setDescription(rs.getString("description"));
+            plo.setCreatedAt(rs.getDate("createdAt").toString());
+            plo.setUpdatedAt(rs.getDate("updatedAt").toString());
         }
+
         con.close();
         return plo;
     }
+
     //lấy list plo theo curriculumID
     public static List<PLO> readPLOList(String curId) throws Exception {
         String query = "select distinct [id], [name], [description], [createdAt], [updatedAt] from Program_Learning_Objective inner join CLO_to_PLO_from_Cur on id = PLO_ID where curriculumID = '?'";
         List<PLO> list = null;
         Connection con = DBUtils.makeConnection();
-        try {
-            PreparedStatement pre = con.prepareStatement(query);
-            pre.setString(1, curId);
-            ResultSet rs = pre.executeQuery();
-            while (rs.next()) {
-                PLO plo = new PLO();
-                plo.setId(rs.getInt("id"));
-                plo.setName(rs.getString("name"));
-                plo.setDescription(rs.getString("description"));
-                plo.setCreatedAt(rs.getDate("createdAt").toString());
-                plo.setUpdatedAt(rs.getDate("updatedAt").toString());
-                list.add(plo);
-            }
-        } catch (Exception e) {
-            System.out.println("Cant get list");
+
+        PreparedStatement pre = con.prepareStatement(query);
+        pre.setString(1, curId);
+        ResultSet rs = pre.executeQuery();
+        while (rs.next()) {
+            PLO plo = new PLO();
+            plo.setId(rs.getInt("id"));
+            plo.setName(rs.getString("name"));
+            plo.setDescription(rs.getString("description"));
+            plo.setCreatedAt(rs.getDate("createdAt").toString());
+            plo.setUpdatedAt(rs.getDate("updatedAt").toString());
+            list.add(plo);
         }
+
         con.close();
         return list;
     }
