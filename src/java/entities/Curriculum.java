b3ef522123782fa4c6476149bac7885ee973ad8a
shinta79 <giahung112358@gmail.com>
2023-04-06 11:09:50 +0700

    return data for curriculum detail

---
+++ src/java/entities/Curriculum.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author giahu
+ */
+public class Curriculum {
+
+    private int id;
+    private String code;
+    private String name;
+    private String description;
+    private String decisionNo;
+    private String viName;
+    private String createdAt;
+    private String updatedAt;
+
+    public Curriculum() {
+    }
+
+    public Curriculum(int id, String code, String name, String description, String decisionNo, String viName, String createdAt, String updatedAt) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.decisionNo = decisionNo;
+        this.viName = viName;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDecisionNo() {
+        return decisionNo;
+    }
+
+    public void setDecisionNo(String decisionNo) {
+        this.decisionNo = decisionNo;
+    }
+
+    public String getViName() {
+        return viName;
+    }
+
+    public void setViName(String viName) {
+        this.viName = viName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
