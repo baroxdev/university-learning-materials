451377aa70cf978d84fab29c0289b712afeb4665
shinta79 <giahung112358@gmail.com>
2023-04-08 11:32:57 +0700

    Revert "Revert "created new exceptions""
    This reverts commit 3ee709d8319fbed23aa551c15996395eb26ae37e.

---
+++ src/java/exceptions/SyllabusException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author giahu
+ */
+public class SyllabusException extends Exception {
+
+    public SyllabusException(String message) {
+        super(message);
+    }
+
+}
