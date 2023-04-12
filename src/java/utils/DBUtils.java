273a0bdf8242b570560d417e6d3f2a73fd83aea7
Barox Phan <75582570+baroxFPTU@users.noreply.github.com>
2023-04-12 20:44:27 +0700

    update connect string

--- src/java/utils/DBUtils.java
+++ src/java/utils/DBUtils.java
@@ -11,10 +11,19 @@ public class DBUtils {
 
     public static Connection makeConnection() throws Exception {
         Connection cn = null;
-
+//        String IP="localhost";
+//        String instanceName="BAROX-LAP\\BAROX2";
+//        String port="1433";
+//        String uid="sa";
+//        String pwd="12345";
+//        String db="ULMDatabase";
+//        String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//        String url= "jdbc:sqlserver://localhost:1433;databaseName=ULMDatabase";
+//        String url="jdbc:sqlserver://" +IP+"\\"+ instanceName+":"+port 
+//                 +";databasename="+db+";user="+uid+";password="+pwd;
         String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String password = "Bao19082002.";
-        String url = "jdbc:sqlserver://plantee.database.windows.net:1433;database=ULMDatabase;user=baroxdev@plantee;password=" + password + ";encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
+        String password = "Fullstack123";
+        String url = "jdbc:sqlserver://ulm-server.database.windows.net:1433;database=ULMDatabase;user=ulmteam@ulm-server;password="+password + ";encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         Class.forName(driver);
         cn = DriverManager.getConnection(url);
         return cn;
