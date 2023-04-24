2fa6d25ae975a7142bdc34cf9ed9ebf8fd3cf74c
shinta79 <giahung112358@gmail.com>
2023-04-24 11:26:19 +0700

    Update CurriculumDao.java

--- src/java/dao/CurriculumDao.java
+++ src/java/dao/CurriculumDao.java
@@ -183,10 +183,10 @@ public class CurriculumDao {
             con.setAutoCommit(false);
 
             if (poList.isEmpty()) {
-                throw new IllegalArgumentException("Atleast one PO must be add.");
+                throw new IllegalArgumentException("PO List is empty.");
             }
             if (ploList.isEmpty()) {
-                throw new IllegalArgumentException("Atleast one PLO must be add.");
+                throw new IllegalArgumentException("PLO List is empty.");
             }
 
             curId = add(con, curriculum);
