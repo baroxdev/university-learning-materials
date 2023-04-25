16c73ffdb35c8e417061ab642e97099018ae371d
John Smith <bao1322002@gmail.com>
2023-04-25 18:04:32 +0700

    handle upload excel for sesssions, question and asssessment

---
+++ src/java/controllers/Auth/AuthPassWordController.java
@@ -0,0 +1,115 @@
+/*
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+     * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controllers.auth;
+
+import actions.auth.UpdatePassword;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import actions.Action;
+
+/**
+ *
+ * @author quocb
+ */
+@WebServlet(name = "Auth2", urlPatterns = {"/auth/*"})
+public class AuthPassWordController extends HttpServlet {
+
+    private Map<String, Action> actionMap = new HashMap<>();
+
+    public void init() {
+        actionMap.put("/auth/reset-password", new UpdatePassword());
+        actionMap.put("/auth/update-password", new UpdatePassword());
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Auth2 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getServletPath() + request.getPathInfo();
+        Action action = actionMap.get(path);
+        System.out.println("path " + path);
+        if (action != null) {
+            action.doGet(request, response);
+        } else {
+            // Handle error if the path is not supported
+            System.out.println("Not found action");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println(request.getContextPath());
+        String path = request.getServletPath() + request.getPathInfo();
+        Action action = actionMap.get(path);
+        System.out.println("path " + path);
+        if (action != null) {
+            action.doPost(request, response);
+        } else {
+            // Handle error if the path is not supported
+            System.out.println("Not found action");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
